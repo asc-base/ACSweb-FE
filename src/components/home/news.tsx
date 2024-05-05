6042b365a0da68ea1e7a65a9143a98bac773cdda
nanthawatsk <jamesukkam@gmail.com>
2024-05-06 00:31:35 +0900

    feature/homepage

--- src/components/home/news.tsx
+++ src/components/home/news.tsx
@@ -1,16 +1,7 @@
 import { Typography } from '@mui/material'
-import { styled } from '@mui/material/styles'
 import NewsCard from './newsCard'
 
 export default function News() {
-  const Title = styled(Typography)`
-    && {
-      margin: 0;
-      font-weight: bold;
-      font-size: 24px;
-      color: #e8360e;
-    }
-  `
   return (
     <>
       <Typography variant="h2" color="secondary" fontWeight="bold">
