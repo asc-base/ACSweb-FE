1f2577503381785d67a336b99c69f42c7d4d502d
Jenesix <acearcadia789@gmail.com>
2024-05-10 23:19:06 +0700

    Change Password Edit_1

---
+++ src/pages/password-change/index.tsx
@@ -0,0 +1,16 @@
+import ForgotPassword from '@/components/ChangePassword/ChangePassword'
+import Head from 'next/head'
+import React from 'react'
+
+const ChangePasswordPage: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <ForgotPassword />
+    </>
+  )
+}
+
+export default ChangePasswordPage
